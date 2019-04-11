efc400ed4cf4acef6454eb7f8ca6f7d3c7b02255
cubsfan46350 <cubsfan.46350@msn.com>
2019-04-11 13:49:22 -0500

    created dashboard and centered content

---
+++ baseball_stadium/src/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div class = "dashboard">
+            <h1>BaseBall Stadium Dashboard</h1>
+        </div>
+    )
+}
+
+export default Dashboard;
